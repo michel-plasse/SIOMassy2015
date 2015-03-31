84646ab0fe0a9a866dc7e0d25482643b9bb3c978
Michel Plasse <mplasse@free.fr>
2015-03-31 10:46:16 +0200

    Revoie Evaluation_m

--- application/models/Evaluation_m.php
+++ application/models/Evaluation_m.php
@@ -1,10 +1,23 @@
 <?php
-class Evaluation_m extends CI_Model{
 
-    public function __construct() {
-        parent::__construct();
-        $this->load->database();
-    }
+class Evaluation_m extends CI_Model {
+
+  public function __construct() {
+    parent::__construct();
+    $this->load->database();
+  }
+
+  /** Evaluations de la session $idSession.
+   * Renvoie un tableau de tableaux associatifs
+   * (id, id_module, nom_module, date,
+   * id_formateur, prenom_formation, nom_formateur).
+   * Tableau vide si aucune évaluation trouvée.
+   */
+  public function getBySession($idSession) {
+    $sql = "SELECT id_evaluation, m.id_module, m.nom AS nom_module,
+            date, ";
+    $query = $this->db->query($sql);
+    return $query->result_array();
+  }
+
 }
-
-

e155d2658f24003b0d0cf8c167dc1b48fec1c202
Joel <Joel@JOEL>
2015-04-17 18:16:14 +0200

    Saisie des notes presque complète updateNoite dans  modele doit encore choisir soit update, soit insert)

--- application/models/evaluation_m.php
+++ application/models/evaluation_m.php
@@ -2,13 +2,13 @@
 
 class Evaluation_m extends CI_Model {
 
-  public function __construct() {
-    parent::__construct();
-    $this->load->database();
-  }
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
+    }
 
-  /** Requete fournissant les données (en-têtes) d'évaluations */
-  const SQL_EN_TETES = "SELECT e.id_evaluation, m.id_module, m.nom AS nom_module,
+    /** Requete fournissant les données (en-têtes) d'évaluations */
+    const SQL_EN_TETES = "SELECT e.id_evaluation, m.id_module, m.nom AS nom_module,
     e.date_effet, e.id_formateur, p.prenom AS prenom_formateur,
     p.nom AS nom_formateur
     FROM evaluation e INNER JOIN module m
@@ -16,30 +16,30 @@ class Evaluation_m extends CI_Model {
     INNER JOIN personne p
     ON e.id_formateur = p.id_personne";
 
-  /** Evaluations de la session $idSession.
-   * Renvoie un tableau de tableaux associatifs
-   * (id_evaluation, id_module, nom_module, date_effet,
-   * id_formateur, prenom_formateur, nom_formateur).
-   * Tableau vide si aucune évaluation trouvée.
-   */
-  public function getBySession($idSession) {
-    $sql = Evaluation_m::SQL_EN_TETES;
-    $query = $this->db->query($sql);
-    return $query->result_array();
-  }
+    /** Evaluations de la session $idSession.
+     * Renvoie un tableau de tableaux associatifs
+     * (id_evaluation, id_module, nom_module, date_effet,
+     * id_formateur, prenom_formateur, nom_formateur).
+     * Tableau vide si aucune évaluation trouvée.
+     */
+    public function getBySession($idSession) {
+        $sql = Evaluation_m::SQL_EN_TETES;
+        $query = $this->db->query($sql);
+        return $query->result_array();
+    }
 
-  /** Evaluation de id $idEval */
-  public function getById($idEval) {
-    $sql = Evaluation_m::SQL_EN_TETES . " WHERE id_evaluation = $idEval";
-    $query = $this->db->query($sql);
-    return $query->row_array();
-  }
+    /** Evaluation de id $idEval */
+    public function getById($idEval) {
+        $sql = Evaluation_m::SQL_EN_TETES . " WHERE id_evaluation = $idEval";
+        $query = $this->db->query($sql);
+        return $query->row_array();
+    }
 
-  /** Notes de l'évaluation de id $idEval.
-   *  Fournit id_stagiaire, prenom, nom, note.
-   */
-  public function getNotesByIdEval($idEval) {
-    $sql = "SELECT s.id_personne AS id_stagiaire, s.prenom, s.nom, n.note
+    /** Notes de l'évaluation de id $idEval.
+     *  Fournit id_stagiaire, prenom, nom, note.
+     */
+    public function getNotesByIdEval($idEval) {
+        $sql = "SELECT s.id_personne AS id_stagiaire, s.prenom, s.nom, n.note
       FROM stagiaire s LEFT OUTER JOIN
       (
         SELECT id_personne, note
@@ -54,8 +54,22 @@ class Evaluation_m extends CI_Model {
           FROM evaluation
           WHERE id_evaluation = $idEval
       )";
-    $query = $this->db->query($sql);
-    return $query->result_array();
-  }
+        $query = $this->db->query($sql);
+        return $query->result_array();
+    }
+
+    /** Met a jour la note du stagiaire dans l'évaluation indiquée, ou
+     * l'insère s'il n'y en a pas
+     * 
+     */
+    public function updateNote($id_stagiaire, $id_evaluation, $note) {
+        $sql = "update note
+            set note = $note
+            where id_personne = $id_stagiaire AND id_evaluation=$id_evaluation";
+        $sql ="INSERT INTO note(id_personne, id_evaluation, note)
+               VALUES ($id_stagiaire, $id_evaluation, $note)";
+        $query = $this->db->query($sql);
+        return $query;
+    }
 
 }

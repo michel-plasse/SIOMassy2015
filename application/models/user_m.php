54da8acb36b87c3a1ea901493150b73719b046cf
Joel <Joel@JOEL>
2015-04-21 14:51:59 +0200

    Connexion va maintenant vérifier dans la BD

--- application/models/user_m.php
+++ application/models/user_m.php
@@ -2,23 +2,35 @@
 
 class User_m extends CI_Model {
 
-  private $email;
+    private $email;
 
-  public function __construct() {
-    parent::__construct();
-    $this->load->database();
-  }
-
-  /** Renvoie un utilisateur ou null, si pas trouvé
-   * ATTENTION : fonctionne seulement avec admin/GretaMassy
-   */
-  public static function getByEmailPassword($email, $pwd) {
-    $result = null;
-    if ($email === "admin" && $pwd === "GretaMassy") {
-      $result = new User_m();
-      $result->email = $email;
+    public function __construct() {
+        parent::__construct();
+        $this->load->database();
     }
-    return $result;
-  }
+
+    /** Renvoie un utilisateur sous forme d'un 
+     * array(id_personne, nom, prenom, email, is_formateur, is_stagiaire), 
+     * ou null, si pas trouvé
+     */
+    public function getByEmailPassword($email, $pwd) {
+        $result = null;
+        $sql = "SELECT 
+            p.id_personne, p.nom, p.prenom, p.email,
+            (f.id_formateur IS NOT NULL) AS is_formateur,
+            (COUNT(s.id_stagiaire) <> 0) AS is_stagiaire
+            FROM personne p
+            LEFT OUTER JOIN formateur f
+                    ON p.id_personne = f.id_formateur
+            LEFT OUTER JOIN stagiaire s
+                    ON p.id_personne = s.id_stagiaire
+            WHERE p.email=? AND p.mot_passe=?
+            GROUP BY p.id_personne, p.nom, p.prenom, p.email;";
+        $query = $this->db->query($sql, array($email, $pwd));
+        if ($query->num_rows() > 0) {
+            $result = $query->row_array();
+        }
+        return $result;
+    }
 
 }

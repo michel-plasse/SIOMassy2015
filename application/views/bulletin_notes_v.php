59ad8be6b15ddeacbeffb7c909b556bc8afa527e
Stéphanie <Stphanie@MoaPC>
2015-05-05 16:59:36 +0200

    modification

---
+++ application/views/bulletin_notes_v.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<?php
+session_start();
+foreach ($stagiaire as $infos_stagiaire) {
+    $nom_stagiaire = $infos_stagiaire["nom_stagiaire"];
+    $prenom_stagiaire = $infos_stagiaire["prenom_stagiaire"];
+    $diplome = $infos_stagiaire["diplome"];
+    $date_conseil = $infos_stagiaire["date_bilan"];
+    $date_naissance = $infos_stagiaire["date_naissance"];
+    $annee_diplome = $infos_stagiaire["annee"];
+    $avis_proviseur = $infos_stagiaire["avis_proviseur"];
+    $id_bilan = $infos_stagiaire["id_bilan"];
+    $id_stagiaire = $infos_stagiaire["id_stagiaire"];
+    $id_module = $infos_stagiaire["id_module"];
+    $id_formateur = $infos_stagiaire["id_formateur"];
+}
+foreach ($moyennes as $moyenne) {
+    $moyenne_stagiaire = $moyenne["moyenne_stagiaire"];
+    $moyenne_classe = $moyenne["moyenne_classe"];
+}
+?>
+
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="<?= $cssUrl ?>"/>
+        <title>Affichage d'un bulletin de notes</title>
+    </head>
+    <body>
+        <main>
+            <?php
+            $this->load->view('header_v');
+            ?>
+            <?php
+            $this->load->view('navigation_v');
+            ?>
+            <section>
+                <header id = "bulletin">
+                    <h1 id = "diplome">Diplome préparé <?php echo $diplome ?></h1>
+                    <p id = "annee">Année : <?php echo $annee_diplome ?></p>
+                    <p id = "date_conseil">Date du conseil de classe : <?php echo date_format(date_create($date_conseil), 'd-m-Y') ?></p>
+                    <p id = "nom">Prénom Nom : <?php echo $prenom_stagiaire . " " . $nom_stagiaire ?></p>
+                    <p id = "date">Date de naissance : <?php echo date_format(date_create($date_naissance), 'd-m-Y') ?> </p>
+                </header>
+                <table id = "notes">
+                    <thead>
+                        <tr>
+                            <th id = 'matiere' rowspan="2">Matières</th>
+                            <th id = 'enseignant' rowspan="2">Nom des enseignants</th>
+                            <th id = 'stagiaire' rowspan="2">Moyennes du stagiaire</th>
+                            <th colspan="3">Moyennes du groupe</th>
+                            <th id = 'commentaire' rowspan="2">Commentaires</th>
+                        </tr>
+                        <tr>
+                            <th class = 'moyenne'> - < </th>
+                            <th class = 'moyenne'>Moyenne</th>
+                            <th class = 'moyenne'> > + </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        foreach ($stagiaire as $info) {
+                            echo "<tr>";
+                            echo "<td>$info[matiere]</td>";
+                            echo "<td>$info[prenom_formateur] $info[nom_formateur]</td>";
+                            echo "<td>$info[moyenne]</td>";
+                            echo "<td>$info[min_classe]</td>";
+                            echo "<td>$info[moyenne_classe]</td>";
+                            echo "<td>$info[max_classe]</td>";
+                            echo "<td><form method='POST'>
+                                        <input type='hidden' name='id_formateur' value=$info[id_formateur]/>
+                                        <input type='hidden' name='id_bilan' value=$info[id_bilan]/>
+                                        <input type='hidden' name='id_module' value=$info[id_module]/>
+                                        <input type='hidden' name='id_stagiaire' value=$info[id_stagiaire]/>
+                                        <textarea name='commentaire' rows='1' cols='150'>$info[avis_prof]</textarea><button type='submit' name='ligne_b'>Valider</button></form></td>";
+                        }
+                        ?>
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <th colspan="2">Moyenne générale</th>
+                            <th><?php echo $moyenne_stagiaire ?></th>
+                            <th></th>
+                            <th><?php echo $moyenne_classe ?></th>
+                            <th colspan="2"></th>
+                        </tr>
+                    </tfoot>
+                </table>
+                <table id = "appréciation">
+                    <thead>
+                        <tr>
+                            <th>Appréciation globale</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <form method="POST">
+                        <input type="hidden" name="id_stagiaire" value="<?= $id_stagiaire ?>"/>
+                        <input type="hidden" name="id_bilan" value="<?= $id_bilan ?>"/>
+                        <tr>
+                            <td>
+                                <input type="text" name="commentaire" value="<?= $avis_proviseur ?>">
+                                <button type="submit" name="bulletin">Valider</button>
+                            </td>
+                        </tr>
+                    </form>
+                    </tbody>
+                </table>
+                <table id = "signature">
+                    <thead>
+                        <tr>
+                            <th>Cachet et signature du responsable d'établissement</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </section>
+            <footer>
+                <p>Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+            </footer>
+        </main>
+    </body>
+</html>
+
--- application/views/bulletins_notes_v.php
+++ application/views/bulletins_notes_v.php
@@ -2,6 +2,7 @@
     <head>
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="<?= $cssUrl ?>"/>
+        <script type="text/javascript" src = "<?= $biblioJSUrl ?>"></script>
         <title>Liste des stagaires</title>
     </head>
     <body>
@@ -15,9 +16,23 @@
             <header id = "liste_bilans" method = "POST">
                 <h1>Liste des stagiaires</h1>
             </header>
-            <?php
-            
-            ?>
+            <form method = "GET" id="formulaire_bulletin" name="formulaire_bulletin" action = "javascript: afficherBulletin();">
+                <label id="bilan">Conseil de classe : </label>
+                <?= form_dropdown('id_bilan', $conseils); ?>
+                <label id="stagiaire">Stagiaire : </label>
+                <?= form_dropdown('id_stagiaire', $stagiaires); ?>
+                <button type="submit">Valider</button>
+                <input type ="hidden" name ="id_session" value ="$id_session"/>
+            </form>
+            <script>
+                function afficherBulletin() {
+                    var href = "/gestionmassy/bulletin_notes/index/"
+                        + <?= $id_session ?> + "/"
+                        + document.formulaire_bulletin.id_bilan.value + "/"
+                        + document.formulaire_bulletin.id_stagiaire.value;
+                   location.href = href;
+                }
+            </script>
         </section>
     </body>
 </html>

9472aa48c2717fc22f9baf21d8f006e3fd711c8d
Michel Plasse <mplasse@free.fr>
2015-05-06 10:00:27 +0200

    Met le charset utf8 dans bilan_v

--- application/views/bilan_v.php
+++ application/views/bilan_v.php
@@ -1,16 +1,17 @@
 <html>
-    <head>
-        <title>Bilan d'un conseil de classe</title>
-        <link rel="stylesheet" type="text/css"
-              href="/gestionmassy/items/css/allproject.css"/>
-    </head>
-    <body>
-        <section>
-            <?php
-            $this->load->view('header_v');
-            $this->load->view('navigation_v');
-            ?>
-            <h1>Bilan du conseil de classe <?= $bilan ?></h1>
-        </section>
-    </body>
+  <head>
+    <title>Bilan d'un conseil de classe</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css"
+          href="/gestionmassy/items/css/allproject.css"/>
+  </head>
+  <body>
+    <section>
+      <?php
+      $this->load->view('header_v');
+      $this->load->view('navigation_v');
+      ?>
+      <h1>Bilan du conseil de classe <?= $bilan ?></h1>
+    </section>
+  </body>
 </html>
